919c46a3e0119e7f17fb2dd5966761c2a887200e
joyaseduardo <joyaseduardo@gmail.com>
2026-03-06 18:56:12 -0300

    Add files via upload

--- anillos-mujer.js
+++ anillos-mujer.js
@@ -1,10 +1,10 @@
 var DATA=[{"codigo": "8501"}, {"codigo": "8502"}, {"codigo": "8503"}, {"codigo": "8504"}, {"codigo": "8505"}, {"codigo": "8506"}, {"codigo": "8507"}, {"codigo": "8508"}, {"codigo": "8509"}, {"codigo": "8510"}, {"codigo": "8511"}, {"codigo": "8512"}, {"codigo": "8513"}, {"codigo": "8514"}, {"codigo": "8515"}, {"codigo": "8516"}, {"codigo": "8517"}, {"codigo": "8518"}, {"codigo": "8519"}, {"codigo": "8520"}, {"codigo": "8521"}, {"codigo": "8522"}, {"codigo": "8523"}, {"codigo": "8524"}, {"codigo": "8525"}, {"codigo": "8526"}, {"codigo": "8527"}, {"codigo": "8528"}, {"codigo": "8529"}, {"codigo": "8530"}, {"codigo": "8531"}, {"codigo": "8532"}, {"codigo": "8533"}, {"codigo": "8534"}, {"codigo": "8535"}, {"codigo": "8536"}, {"codigo": "8537"}, {"codigo": "8538"}, {"codigo": "8539"}, {"codigo": "8540"}, {"codigo": "8541"}, {"codigo": "8542"}, {"codigo": "8543"}, {"codigo": "8544"}];
-var CARPETA="https://res.cloudinary.com/dfsnzdwqx/image/upload";
+var CARPETA="imagenes/ORO-18K-ESPECIALES-CON-PIEDRAS";
 var NOMBRE="Anillo Oro Mujer";
 var CAT="Anillos Oro Mujer";
 var WA="5492976235421";
 
-function renderGrid() {
+document.addEventListener("DOMContentLoaded", function() {
   var grid = document.getElementById("grid");
   if (!grid) return;
   var html = "";
@@ -12,7 +12,7 @@ function renderGrid() {
     var cod = DATA[i].codigo;
     html += "<div class='card'>"
           + "<div class='card-foto'>"
-          + "<img src='" + CARPETA + "/" + cod + ".png' alt='" + cod + "' onerror='this.style.display=\"none\";this.nextSibling.style.display=\"flex\"'>"
+          + "<img src='" + CARPETA + "/" + cod + ".jpg' alt='" + cod + "' onclick='abrirLightbox(this.src,this.alt)' onerror='this.style.display=\"none\";this.nextSibling.style.display=\"flex\"'>"
           + "<div class='placeholder' style='display:none'><span>&#x1F48D;</span><p>" + cod + "</p></div>"
           + "</div>"
           + "<div class='card-body'>"
@@ -29,9 +29,7 @@ function renderGrid() {
           + "</div></div>";
   }
   grid.innerHTML = html;
-}
-
-window.onload = renderGrid;
+});
 
 function waMsg(tipo, cod) {
   var base = "Codigo: " + cod + " (" + CAT + ")";
